cc7d0ed13b09d8bfd049c7f5a1541cfad6be41c0
New Slave <banana13373228@gmail.com>
2022-03-07 18:47:22 +0300

    fff

---
+++ public/js/900.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[900],{6900:(e,n,l)=>{l.r(n),l.d(n,{default:()=>t});const s={name:"Personal"};const t=(0,l(1900).Z)(s,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n   personal\n")])}),[],!1,null,null,null).exports}}]);
